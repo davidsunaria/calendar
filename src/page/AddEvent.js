7901f5a1e798a84732aae77d8a685a416faeec11
Praveen Soni <praveen@shinewebservices.com>
2022-01-27 19:37:11 +0530

    addevent

---
+++ src/page/AddEvent.js
@@ -0,0 +1,37 @@
+import {useState} from "react"
+import { Card, Button, ListGroup, Modal, Form } from 'react-bootstrap'
+
+function AddEvent(props) {
+
+
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div className="App">
+     <Modal
+        show={props.toggle}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default AddEvent;
--- src/page/Appoitment.js
+++ src/page/Appoitment.js
@@ -2,36 +2,51 @@
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
+import AddEvent from "./AddEvent.js"
+import { useState } from 'react';
 
 const localizer = momentLocalizer(moment);
 
 
-const selectedSlot = ({start,end})=>{
-console.log("e",start,end)
-}
 
-
-
-let eventlist= [
-  {
-    title: "demo",
-    allDay:true,
-    start: new Date(2022, 0, 29),
-    end: new Date(2022, 0, 32),
-  },
-  {
-    title: "demo2",
-    allDay:true,
-    start: new Date(2022, 1, 3),
-    end: new Date(2022, 1, 9),
-  }
-
-] 
 
 function Appoitment() {
+
+
+  const [showEvent, setShowEvent] = useState(false)
+
+  const selectedSlot = ({start,end})=>{
+    console.log("e",start,end)
+    setShowEvent(true)
+    }
+    
+    
+    const handleSelectEvent = (e)=>{
+      console.log("e",e)
+      
+      }
+    
+     
+    
+    let eventlist= [
+      {
+        title: "demo",
+        start: new Date(2022, 0, 29),
+        end: new Date(2022, 0, 32),
+      },
+      {
+        title: "demo2",
+        start: new Date(2022, 1, 3),
+        end: new Date(2022, 1, 9),
+      }
+    
+    ] 
+
+
   return (
     <div className="App">
-     <h1 className="text-info">React Big Calendar</h1>
+     <h1 className="text-info">React Big Calendar</h1><br/><br/>
+         <AddEvent toggle={showEvent}/> 
      <Calendar
       selectable
       style={{ height: 700 }}
@@ -44,6 +59,7 @@ function Appoitment() {
       onSelectSlot={selectedSlot}
       step={5}
       timeslots={4}
+      onSelectEvent={handleSelectEvent}
     />
     </div>
   );
