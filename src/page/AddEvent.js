42094eb936d779348acc753d1d8c475688a8ce16
david <er.daviddabwali@gmail.com>
2022-01-30 22:50:39 +0530

    home

--- src/page/AddEvent.js
+++ src/page/AddEvent.js
@@ -6,27 +6,38 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function AddEvent(props) {
 
-  const [eventDetail, setEvent] = useState({
-   
-  });
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const [eventName, setEventName] = useState(""
+
+  );
 
   const handleClose = () => props.closeModal(false);
 
 
   const onchangehander = (event) => {
-    setEvent(
-      {
-          ...eventDetail, [event.target.name]: event.target.value
-      }
-  )
+    setEventName(
+
+      event.target.value
+
+    )
   }
 
   function submit(event) {
     event.preventDefault()
-    console.log("event",event)
-    props.setEventList(eventDetail)
-  
-}
+    console.log("event", event)
+    props.setEventList({
+      title: eventName,
+      start: startDate,
+      end: endDate,
+    })
+    props.closeModal(false)
+  }
+
+  const handleDateSelect = (e) => {
+    console.log(e)
+  }
 
   return (
     <>
@@ -43,17 +54,17 @@ function AddEvent(props) {
           <Form onSubmit={submit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Title</Form.Label>
-              
-              <Form.Control type="text" name="title" placeholder="Enter Title" value={eventDetail.title} onChange={onchangehander}/> 
+
+              <Form.Control type="text" name="title" placeholder="Enter Title" value={eventName} onChange={onchangehander} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Start Date</Form.Label>
-              <Form.Control type="text" placeholder="8/10/22" name="start" value={eventDetail.start} onChange={onchangehander}/>
+              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} onSelect={handleDateSelect} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Label>End Date</Form.Label>
-              <Form.Control type="text" placeholder="9/10/22" name="end" value={eventDetail.end} onChange={onchangehander}/>
+              <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} onSelect={handleDateSelect} />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
--- src/page/Appoitment.js
+++ src/page/Appoitment.js
@@ -17,55 +17,49 @@ function Appoitment() {
   const [year, setYear] = useState(null)
   const [day, setDay] = useState(null)
   const [month, setMonth] = useState(null)
-  const [eventlist, setEventList] = useState([
-    {
-      title: "demo",
-      start: "Sat Jan 01 2022 00:00:00 GMT+0530 (India Standard Time)",
-      end: new Date("01/5/2022"),
-    }
-  ] )
+  const [eventlist, setEventList] = useState([])
 
-console.log("eventlist",eventlist)
+  console.log("eventlist", eventlist)
 
-const setNewEvent = (data)=>{
-    let oldEvent=  [...eventlist]
+  const setNewEvent = (data) => {
+    let oldEvent = [...eventlist]
     oldEvent.push(data)
     setEventList(oldEvent)
   }
-console.log("old",eventlist)
-  const selectedSlot = ({start,end})=>{
-    console.log("e",start,end)
+  console.log("old", eventlist)
+  const selectedSlot = ({ start, end }) => {
+    console.log("e", start, end)
     setShowEvent(true)
-    }
-    
-    
-    const handleSelectEvent = (e)=>{
-      console.log("e",e)
-      
-      }
-    
-     
-  
+  }
+
+
+  const handleSelectEvent = (e) => {
+    console.log("e", e)
+
+  }
+
+
+
 
 
   return (
     <div className="App">
-     <h1 className="text-info">React Big Calendar</h1><br/><br/>
-         <AddEvent toggle={showEvent} closeModal={setShowEvent} setEventList={setNewEvent}/> 
-     <Calendar
-      selectable
-      style={{ height: 700 }}
-      localizer={localizer}
-       defaultView={Views.MONTH}
-       events={eventlist}
-      startAccessor="start"
-      endAccessor="end"
-      views={['month', 'week', 'day']}
-      onSelectSlot={selectedSlot}
-      step={5}
-      timeslots={4}
-      onSelectEvent={handleSelectEvent}
-    />
+      <h1 className="text-info">React Big Calendar</h1><br /><br />
+      <AddEvent toggle={showEvent} closeModal={setShowEvent} setEventList={setNewEvent} />
+      <Calendar
+        selectable
+        style={{ height: 700 }}
+        localizer={localizer}
+        defaultView={Views.MONTH}
+        events={eventlist}
+        startAccessor="start"
+        endAccessor="end"
+        views={['month', 'week', 'day']}
+        onSelectSlot={selectedSlot}
+        step={5}
+        timeslots={4}
+        onSelectEvent={handleSelectEvent}
+      />
     </div>
   );
 }
